4dce7daff7df116ca29c89822ac0928630a87b9b
fmorgan1413 <91088434+fmorgan1413@users.noreply.github.com>
2024-03-05 17:24:06 -0500

    all character assets implemented / start of clue art assets implemented / some bug and player softlock fixes

--- VN_Thesis/Assets/Scripts/buttonFX.cs
+++ VN_Thesis/Assets/Scripts/buttonFX.cs
@@ -1,14 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class buttonFX : MonoBehaviour
 {
     public AudioSource fx;
     public AudioClip hoverFX;
 
+    public GameObject highlight;
+
     public void HoverSound()
     {
         fx.PlayOneShot(hoverFX);
+    }
+
+    private void OnMouseOver()
+    {
+        highlight.SetActive(true);
+    }
+
+    private void OnMouseExit()
+    {
+        highlight.SetActive(false);
     }
 }
